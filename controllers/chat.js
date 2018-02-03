d6a40c46c9897c150911d0618bd9bfc47e784cc2
unknown <985966@CS.MUM.EDU>
2018-02-03 10:30:23 -0600

    rep chat controller

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,69 +1,69 @@
-const User = require('../models/User.js'),
-    Log = require('../models/Log.js'),
-    Group = require('../models/Group.js');
+// const User = require('../models/User.js'),
+//     Log = require('../models/Log.js'),
+//     Group = require('../models/Group.js');
 
-exports.getLogs = (req, res, next) => {  
-    Group.find({ users: req.user._id })
-        .select('_id')
-        .exec((err, groups) => {
-            if (err) {
-                res.send({ error: err });
-                return next(err);
-            }
+// exports.getLogs = (req, res, next) => {  
+//     Group.find({ users: req.user._id })
+//         .select('_id')
+//         .exec((err, groups) => {
+//             if (err) {
+//                 res.send({ error: err });
+//                 return next(err);
+//             }
 
-            let fullLogs = [];
-            groups.forEach((group) => {
-                Log.find({ 'groupId': group._id })
-                    .sort('-createdAt')
-                    .limit(1)
-                    .populate({
-                        path: "user",
-                        select: "profile.username profile.gender"
-                    })
-                    .exec((err, log) => {
-                        if (err) {
-                            res.send({ error: err });
-                            return next(err);
-                        }
-                        fullLogs.push(log);
-                        if(fullLogs.length === logs.length) {
-                            return res.status(200).json({ logs: fullLogs });
-                        }
-                    });
-            });
-        });
-}
+//             let fullLogs = [];
+//             groups.forEach((group) => {
+//                 Log.find({ 'groupId': group._id })
+//                     .sort('-createdAt')
+//                     .limit(1)
+//                     .populate({
+//                         path: "user",
+//                         select: "profile.username profile.gender"
+//                     })
+//                     .exec((err, log) => {
+//                         if (err) {
+//                             res.send({ error: err });
+//                             return next(err);
+//                         }
+//                         fullLogs.push(log);
+//                         if(fullLogs.length === logs.length) {
+//                             return res.status(200).json({ logs: fullLogs });
+//                         }
+//                     });
+//             });
+//         });
+// }
 
-exports.newGroup = function(req, res, next) {  
-    if(!req.params.partiners) {
-      res.status(422).send({ error: 'Please choose a valid' });
-      return next();
-    }
+// exports.newGroup = function(req, res, next) {  
+//     if(!req.params.partiners) {
+//       res.status(422).send({ error: 'Please choose a valid' });
+//       return next();
+//     }
   
-    const group = new Group({
-        users: [req.user._id, ...req.params.recipient]
-    });
+//     const group = new Group({
+//         users: [req.user._id, ...req.params.recipient]
+//     });
   
-    conversation.save(function(err, newConversation) {
-      if (err) {
-        res.send({ error: err });
-        return next(err);
-      }
+//     conversation.save(function(err, newConversation) {
+//       if (err) {
+//         res.send({ error: err });
+//         return next(err);
+//       }
   
-      const message = new Message({
-        conversationId: newConversation._id,
-        body: req.body.composedMessage,
-        author: req.user._id
-      });
+//       const message = new Message({
+//         conversationId: newConversation._id,
+//         body: req.body.composedMessage,
+//         author: req.user._id
+//       });
   
-      message.save(function(err, newMessage) {
-        if (err) {
-          res.send({ error: err });
-          return next(err);
-        }
+//       message.save(function(err, newMessage) {
+//         if (err) {
+//           res.send({ error: err });
+//           return next(err);
+//         }
   
-        res.status(200).json({ message: 'Conversation started!', conversationId: conversation._id });
-        return next();
-      });
-    });
-}
+//         res.status(200).json({ message: 'Conversation started!', conversationId: conversation._id });
+//         return next();
+//       });
+//     });
+// }
